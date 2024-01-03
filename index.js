656412e6b4a1e21ab37fbe833806922cbfc0e81d
Srushti kakade <85052036+SrushtiKakade28@users.noreply.github.com>
2024-01-03 12:30:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+//ReactDOM.render(<h1>Hello,everyone!</h1>,document.getElementById("root"))
+//render means place something on the screen
+//document.getelementbyid is appending the heading
+//ReactDOM.render(<ul><li>skna</li><li>akfiid</li></ul>,document.getElementById("root"))
+//react is composable
+//function MainContent(){
+    //return (<h1>im learning react</h1>)}
+//ReactDOM.render(<div> <Navbar /><MainContent/></div>,document.getElementById("root"))
+
+//react is imperative
+//ReactDOM.render(<h1 className="header">Hello,everyone!</h1>,document.getElementById("root"))
