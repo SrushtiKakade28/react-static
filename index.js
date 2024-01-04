55c1b4fe90324c6753fe78402451cb66adfde640
Srushti kakade <85052036+SrushtiKakade28@users.noreply.github.com>
2024-01-04 17:05:56 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+
+
 //ReactDOM.render(<h1>Hello,everyone!</h1>,document.getElementById("root"))
 //render means place something on the screen
 //document.getelementbyid is appending the heading
@@ -9,3 +14,24 @@
 
 //react is imperative
 //ReactDOM.render(<h1 className="header">Hello,everyone!</h1>,document.getElementById("root"))
+
+
+//jsx javascript xml (html represented as javascript)
+//jsx is almost html but with a few changes like
+//className instead of class
+
+//creating a navbar in jsx
+const nav = (
+        
+        <navbar>
+            <h1>srushti</h1>
+            <ul>
+                <li>Pricing</li>
+                <li>About</li>
+                <li>Contact</li>
+                </ul>
+        </navbar>
+
+
+)
+        ReactDOM.render(nav,document.getElementById("root"))
